48137898b5bfb616f86e143f345115ed58287eb1
Przemysław Sitek <przemo2@gmail.com>
2018-09-09 19:31:09 +0200

    v5

--- src/zad1.java
+++ src/zad1.java
@@ -1,12 +1,12 @@
-public class zad1 {
-    public static void main(String[] args) {
-        System.out.println("Uczę");
-        System.out.println("się");
-        System.out.println("programowania");
-        System.out.print('i');
-        System.out.println("td.");
-        System.out.println("Bo lubię\n" +
-                "programować\n" +
-                "!!! :)");
-    }
-}
+//public class zad1 {
+//    public static void main(String[] args) {
+//        System.out.println("Uczę");
+//        System.out.println("się");
+//        System.out.println("programowania");
+//        System.out.print('i');
+//        System.out.println("td.");
+//        System.out.println("Bo lubię\n" +
+//                "programować\n" +
+//                "!!! :)");
+//    }
+//}
